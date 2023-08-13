697e2a34bd93cb9723033292a450c5336e44846e
moohabir <moohabir@gmail.com>
2023-08-13 20:45:23 +0200

    project built

---
+++ dist/static/index-c3743809-96ecdd3d.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-7415496d-836d0839.js";import"./sanity-7f3fc886.js";import"./json-inspector-1994b8d1.js";export{m as DocumentPaneProvider,a as default};
--- dist/static/index-c3743809-e1db22ae.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as m,DocumentPane as a}from"./index-7415496d-6904401e.js";import"./sanity-3392fcc5.js";import"./json-inspector-6e87ab17.js";export{m as DocumentPaneProvider,a as default};
