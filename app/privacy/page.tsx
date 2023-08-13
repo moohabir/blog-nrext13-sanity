a05b67effae0604133f8f129f33897689edaab31
moohabir <moohabir@gmail.com>
2023-08-13 20:54:48 +0200

    rzt

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -4,6 +4,12 @@ export default function PrivacyPolicy() {
   return (
     <div className="text-center items-center flex justify-center gap-5 flex-col mt-10 pt-10 mb-20">
       Welcome to Privacy Policy
+      <h3>Your data can only be used for website Performance purpose.</h3>
+      <span>You data will not be used in any other purposes</span>
+      <span>
+        Your data will be protected and used in accordance to European Union
+        Laws of data protection
+      </span>
     </div>
   );
 }
